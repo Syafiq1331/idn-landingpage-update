163e20b026d2802b16ce929c049b0a4ab875bc3f
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-06-30 02:20:07 +0700

    fix:add "back to home" anchor link

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -5,11 +5,16 @@ interface NotFoundPageProps { }
 const NotFoundPage: React.FC<NotFoundPageProps> = ({ }) => {
   return (
     <div className='
-      h-screen flex justify-center items-center
+      h-screen flex justify-center items-center flex-col
     '>
       <h1 className='text-4xl font-extrabold text-center'>
         Coming Soon 👋
       </h1>
+      <a href="/">
+        <p className=' text-blue-700 font-semibold hover:underline mt-4 underline'>
+          Back to home
+        </p>
+      </a>
     </div>
   );
 };
